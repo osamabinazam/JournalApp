968db12c31638fe952f943c4fe8cf452cf154110
Osama Bin Azam <osamabinazam@gmail.com>
2024-08-12 01:32:58 +0500

    first commit

---
+++ src/test/java/pk/smartq/journalApp/JournalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pk.smartq.journalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
