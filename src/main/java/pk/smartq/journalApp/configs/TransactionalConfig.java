f601e5c54bfb550d9f2631c2ed9b717ccfdb8134
Osama Bin Azam <osamabinazam@gmail.com>
2024-08-28 22:32:02 +0500

    Enable transaction on method

---
+++ src/main/java/pk/smartq/journalApp/configs/TransactionalConfig.java
@@ -0,0 +1,20 @@
+package pk.smartq.journalApp.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class TransactionalConfig {
+
+
+    // Return The PlatformTransactionalManager
+    @Bean
+    PlatformTransactionManager transactionManager(MongoDatabaseFactory dbFactory){
+        return new MongoTransactionManager(dbFactory);
+    }
+}
